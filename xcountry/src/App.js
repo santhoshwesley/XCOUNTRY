58026e577174ba9ea3f436c443754040d44314ee
santhoshwesley <santhoshwesleyyer@gmail.com>
2024-02-22 11:44:04 +0530

    API implemented

--- xcountry/src/App.js
+++ xcountry/src/App.js
@@ -1,10 +1,32 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+import { useEffect, useState } from "react";
+import axios from "axios";
 function App() {
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("https://restcountries.com/v3.1/all");
+        setCountries(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
   return (
-    <div className="App">
-      <h1>hi</h1>
+    <div className="container">
+      {countries.map((country) => (
+        <div key={country.ccn3} className="flexContainer">
+          <img
+            src={country.flags.png}
+            alt={country.name.common}
+            style={{ height: "100px", width: "100px" }}
+          />
+          <h2>{country.name.common}</h2>
+        </div>
+      ))}
     </div>
   );
 }
